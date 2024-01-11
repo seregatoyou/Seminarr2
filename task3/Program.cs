1092cf7b3be0c13b75c78c3ea0602454f6ce38fd
IT <spanyukhin@list.ru>
2024-01-11 16:38:05 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+void task3 (){
+    int number = new Random().Next(10, 100);
+    Console.WriteLine($"{number}");
+    int part1 = number % 10;
+    int part2 = number / 10;
+    
+    if (part1 > part2){
+        Console.WriteLine($"вторая цифра больше первой");
+    }
+    else if (part2 > part1){
+         Console.WriteLine($"первая цифра больше второй");
+     }
+    else{
+         Console.WriteLine($"цифры числа равны");
+    }
+}
+task3();
