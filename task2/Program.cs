620f261e5e0e7a1d5c3d3a90fa618af58f8e4cb3
IT <spanyukhin@list.ru>
2024-01-11 15:41:46 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+void task2 (int x, int y){
+    if(x>0 && y>0){
+        Console.WriteLine($"точка в первой четверти");
+    }
+    else if(x<0 && y>0){
+        Console.WriteLine($"точка во второй четверти");
+    }
+    else if(x<0 && y<0){
+        Console.WriteLine($"точка в третьей четверти");
+    }
+    else if(x>0 && y<0){
+        Console.WriteLine($"точка в четвертой четверти");
+    }
+}
+task2(-1, 1);
