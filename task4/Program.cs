c67187d385ead70a693eb74e270acbcc19e7861a
IT <spanyukhin@list.ru>
2024-01-11 16:53:45 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+void task4 (int number){
+    int part2 = 1;
+      while (part2 !=0){
+        int part1 = number % 10;
+        part2 = number / 10;
+        if (part2 == 0){
+            Console.Write($"{part1}. ");
+        }
+        else{
+            Console.Write($"{part1}, ");
+        }
+        number = part2;
+    }
+}
+task4(123);
