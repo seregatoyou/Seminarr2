9cb3b37e9aa9984c5c61b58e11bdb631fef59601
IT <spanyukhin@list.ru>
2024-01-11 15:24:30 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+void task (int number){
+    if (number % 7 == 0 && number % 23 == 0){
+        Console.WriteLine($"Число одновременно кратно 7и и 23м.");
+    }
+    else if (number % 7 == 0){
+        Console.WriteLine($"Число кратно 7и.");
+    }
+    else if (number % 23 == 0){
+        Console.WriteLine($"Число кратно 23м.");
+    }
+    else {
+        Console.WriteLine($"Число не кратно ни 7и, ни 23м");
+    }
+}
+task(41);
